18e07aef13b8337b43d786768f846ec418338a6e
LONOAL <lnogueirapan@danielcastelao.org>
2022-10-11 10:04:30 +0200

    exemplo obx

--- exemploObxectos/src/main/java/com/mycompany/exemploobxectos/ExemploObxectos.java
+++ exemploObxectos/src/main/java/com/mycompany/exemploobxectos/ExemploObxectos.java
@@ -2,7 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
  */
-
 package com.mycompany.exemploobxectos;
 
 /**
@@ -12,10 +11,17 @@ package com.mycompany.exemploobxectos;
 public class ExemploObxectos {
 
     public static void main(String[] args) {
-       
-        Rectangulo obxRec1= new Rectangulo();
-        float resultado= obxRec1.calcularArea(2,3.0f);
-        System.out.println("area= "+resultado);
+
+        Rectangulo obxRec1 = new Rectangulo();
+        float resultado = obxRec1.calcularArea(2, 3.0f);
+        System.out.println("area= " + resultado);
         obxRec1.calcularPerimetro(2, 3);
+        //instancio obxecto de tipo rectangulo co constructor parametrizado
+        Rectangulo obxRec2 = new Rectangulo(5, 8);
+        float base = obxRec2.getBase();
+        float altura = obxRec2.getAltura();
+        float result = obxRec2.calcularArea(base, altura);
+        System.out.println("o novo area= " + result);
+
     }
 }
--- exemploObxectos/src/main/java/com/mycompany/exemploobxectos/Rectangulo.java
+++ exemploObxectos/src/main/java/com/mycompany/exemploobxectos/Rectangulo.java
@@ -1,4 +1,3 @@
-
 package com.mycompany.exemploobxectos;
 
 /**
@@ -7,20 +6,49 @@ package com.mycompany.exemploobxectos;
  */
 public class Rectangulo {
     //Declaro atributos
-    
+
     public float base;
     private float altura;
+    public float lado1;
+    public float lado2;
     
+    //constructores 
+    public Rectangulo(float ba, float alt) { //constructor parametrizado
+        base = ba;
+        altura = alt;
+    }
+
+    public Rectangulo() { //constructor por defecto
+
+    }
     //Defino metodos
-    
-    public float calcularArea(float b, float a){
-        float area = b*a ;
+
+    public float calcularArea(float b, float a) {
+        float area = b * a;
         return area;
     }
 
-
-    public void calcularPerimetro(float b, float a){
-        float peri = 2*b+2*a;
-        System.out.println(" perimetro= "+peri);
+    public void calcularPerimetro(float b, float a) {
+        float peri = 2 * b + 2 * a;
+        System.out.println(" perimetro= " + peri);
     }
+       
+//metodo de acceso
+    
+    public void setAltura (float al){
+        altura= al;
+    }
+    public float getAltura(){
+        return altura;
+    }
+    
+    public void setBase (float ba){
+        base= ba;
+    }
+    
+    public float getBase(){
+        return base;
+    }
+     
+    
 }
